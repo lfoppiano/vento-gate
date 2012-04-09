7da50e94767d6353ad082d25ab6a48eaa1e84ea0
Martin <mpolojko@gmail.com>
2012-04-09 17:32:11 +0200

    version with persistent corpus, no mem problems, needs refactoring

--- src/main/java/org/vento/semantic/sentiment/SentiBatchLearning.java
+++ src/main/java/org/vento/semantic/sentiment/SentiBatchLearning.java
@@ -7,6 +7,8 @@ import gate.Factory;
 import gate.Gate;
 import gate.creole.ExecutionException;
 import gate.creole.ResourceInstantiationException;
+import gate.persist.SerialDataStore;
+import gate.util.ExtensionFileFilter;
 import gate.util.GateException;
 import gate.util.persistence.PersistenceManager;
 import org.vento.gate.GateBatchProcessing;
@@ -14,6 +16,7 @@ import org.vento.gate.GateBatchProcessing;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,13 +28,15 @@ import java.net.MalformedURLException;
 public class SentiBatchLearning implements GateBatchProcessing{
 
     CorpusController application;
+    Corpus persistentCorpus;
+    SerialDataStore persistentDS;
 
     public SentiBatchLearning(File gateConfigFile) throws IOException, GateException {
         init(gateConfigFile);
     }
 
-    public SentiBatchLearning(File gateHome, File gateConfigFile) throws IOException, GateException {
-        init(gateHome, gateConfigFile);
+    public SentiBatchLearning(File gateHome, File gateConfigFile, String dataStoreDir, String corpusName) throws IOException, GateException {
+        init(gateHome, gateConfigFile, dataStoreDir, corpusName);
     }
 
     public void init(File gateConfigFile) throws GateException, IOException {
@@ -44,12 +49,26 @@ public class SentiBatchLearning implements GateBatchProcessing{
 
     }
 
-    public void init(File gateHome, File gateConfigFile) throws GateException, IOException {
+    public void init(File gateHome, File gateConfigFile, String dataStoreDir, String corpusName) throws GateException, IOException {
 
         Gate.setGateHome(gateHome);
 
         // initialise GATE - this must be done before calling any GATE APIs
         Gate.init();
+
+        /// /create&open a new Serial Data Store
+        //    pass the datastore class and path as parameteres
+        persistentDS  = (SerialDataStore)Factory.createDataStore("gate.persist.SerialDataStore",dataStoreDir);
+        persistentDS.open();
+
+        Corpus learningCorpus = Factory.newCorpus(corpusName);
+        persistentCorpus = null;
+
+        //save corpus in datastore
+        //    SecurityInfo is ingored for SerialDataStore - just pass null
+        //    a new persisent corpus is returned
+        persistentCorpus = (Corpus)persistentDS.adopt(learningCorpus,null);
+        persistentDS.sync(persistentCorpus);
 
         // load the saved application
         application = (CorpusController) PersistenceManager.loadObjectFromFile(gateConfigFile);
@@ -60,16 +79,26 @@ public class SentiBatchLearning implements GateBatchProcessing{
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    public void loadCorpus(String corpusName, File[] examples) throws ResourceInstantiationException, MalformedURLException {
-        Corpus corpus = Factory.newCorpus(corpusName);
-        for (File file:examples) {
-            corpus.add(Factory.newDocument(file.toURI().toURL()));
-        }
-        application.setCorpus(corpus);
+    public void populateCorpus(URL directory, String extension, String encoding, Boolean recurseDirs) throws IOException, GateException {
+
+        persistentCorpus.populate(directory, new ExtensionFileFilter("XML files",extension),encoding,recurseDirs);
+        persistentDS.sync(persistentCorpus);
+    }
+
+    public void addToCorpus(File file) throws MalformedURLException, GateException {
+
+       persistentCorpus.add(Factory.newDocument(file.toURI().toURL()));
+
+       //Document persistentDoc = persistentDS.adopt(Factory.newDocument(file.toURI().toURL()), null);
+       //persistentDS.sync(persistentDoc);
+
+       persistentDS.sync(persistentCorpus);
 
     }
 
-    public void performTraining() throws ExecutionException {
+    public void performTraining() throws ExecutionException, GateException {
+        application.setCorpus(persistentCorpus);
+        persistentDS.sync(persistentCorpus);
         application.execute();
 
     }
--- src/test/groovy/org/vento/semantic/sentiment/SentiBatchLearningTest.groovy
+++ src/test/groovy/org/vento/semantic/sentiment/SentiBatchLearningTest.groovy
@@ -24,39 +24,48 @@ public class SentiBatchLearningTest {
         
 
         File gateConfigFile = new File(this.getClass().getResource("/batch-learning.training.configuration.xml").toURI());
-        File gateHome = new File("/opt/GATE_7.0");
+        //Fix this path
+        File gateHome = new File("/Applications/GATE_Developer_7.0/");
+        String dataStore = this.getClass().getResource("/").toURI().toString()+"/temp_learning_datastore/";
 
-        batchLearning = new SentiBatchLearning(gateHome, gateConfigFile);
+        batchLearning = new SentiBatchLearning(gateHome, gateConfigFile, dataStore,"learningCorpus");
     }
 
     @Test
     public void testEnd2EndLearning() throws Exception {
-
-        File corpusDirectory = new File("/home/lfoppiano/develop/bi/batch_learning_GATE_resources/realLearningInput");
+        //Fix this path
+        File corpusDirectory = new File("/Users/Martin/Desktop/learningTest/realLearningInput/");
 
         println "start loading"
         int good = 0
         int bad = 0
         int counter = 0
 
+        batchLearning.populateCorpus(corpusDirectory.toURI().toURL(), "xml", "UTF-8", false)
+        /*
         corpusDirectory.eachFile { file ->
 
             //adding the corpus
             counter++
             try{
-                batchLearning.loadCorpus("cristoCheCorpus", file);
+                batchLearning.addToCorpus(file) //loadCorpus("cristoCheCorpus", corpusDirectory.listFiles());
                 good++
-            }
+           }
             catch(e){
                 bad++
                 println e.getStackTrace()
             }
         }
         
-        println "finish loading, total= ${counter}, good = ${good}, bad = ${bad}"
+        println "finished loading, total= ${counter}, good = ${good}, bad = ${bad}"
+        */
+        println "learning session started..."
 
-        // starting learning
+        def before = System.currentTimeMillis()
         batchLearning.performTraining();
+        def after = System.currentTimeMillis()
+
+        println "learning session finished, took ${after-before} ms"
 
     }
     
